7626ab2e5af1a5eea17ebbeb0b142855312227e5
ZaoShang12 <142527679+ZaoShang12@users.noreply.github.com>
2025-01-30 14:08:35 -0600

    setup. Now works (kinda)

--- Clock/server/db_connection.js
+++ Clock/server/db_connection.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql2');
 
 var con = mysql.createConnection({
-  host: "ec2-54-209-177-70.compute-1.amazonaws.com",
-  user: "css",
-  password: "oPPtWFk9r2Ne1PTbHN1z"
+  host: "ec2-3-219-93-177.compute-1.amazonaws.com",
+  user:  "redhawks_css",
+  password: "oPPtWFk9r2Ne1PTbHN1z!"
 });
 
 con.connect(function(err) {
